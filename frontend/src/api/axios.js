b2ef6c92334f6c0f1444b61e17786c4cd2157bc3
Arun Yadav <ay20552291@gmail.com>
2026-02-16 17:35:55 +0530

    sec

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+//https://event-mngmnt-bellcorp.onrender.com/
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+ // baseURL: "http://localhost:5000/api",
+   baseURL: "https://event-mngmnt-bellcorp.onrender.com/api",
 });
 
 export default instance;
